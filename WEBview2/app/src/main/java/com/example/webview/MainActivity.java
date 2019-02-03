bc1864ef8ac306e9ae8c2547024c2863050ad052
RickyXian <43319020+RickyXian@users.noreply.github.com>
2019-02-02 19:25:08 -0500

    APP

---
+++ WEBview2/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.webview;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+    private WebView webView;
+    private Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        bt=(Button) findViewById(R.id.refresh);
+        bt.setOnClickListener(this);
+
+    }
+    public void onClick(View v) {
+        webView.loadUrl("http://192.168.137.236");
+    }
+
+    private void init(){
+        webView = (WebView) findViewById(R.id.webview);
+        //WebView加载web资源
+        webView.loadUrl("http://192.168.137.236");
+        //覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+}
+
